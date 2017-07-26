650228451dbaf7b3ee7cef666961357e3632a6ab
hqweay <1120073230@qq.com>
2017-07-26 11:14:43 +0800

    doneMapHalf

--- TankDemo/MapTest.cs
+++ TankDemo/MapTest.cs
@@ -33,17 +33,7 @@ namespace TankDemo
             //    login.Close();
             //}
 
-            Random ran = new Random();
-            for (int i = 0; i < 20; i++)
-            {
-               
-                int x = ran.Next(48);
-                int y = ran.Next(27);
-                Wall wall = new Wall();
-                wall.setX(x);
-                wall.setY(y);
-                wallList.Add(wall);
-            }
+            
 
 
             InitializeComponent();
@@ -51,7 +41,7 @@ namespace TankDemo
 
         private void MapTest_Load(object sender, EventArgs e)
         {
-            
+            this.crateWall();
             this.Paint(this.CreateGraphics());
         }
 
@@ -59,9 +49,76 @@ namespace TankDemo
         {
             foreach(Wall wall in wallList)
             {
-                g.FillRectangle(new SolidBrush(Color.Green), wall.getX(), wall.getY(), wall.getX() + Wall.WALL_SIZE, wall.getY() + Wall.WALL_SIZE);
+                switch (wall.getType())
+                {
+                    case 0:
+                        g.FillRectangle(new SolidBrush(Color.Green), wall.getX() * 40, wall.getY() * 40, Wall.WALL_SIZE, Wall.WALL_SIZE);
+                        break;
+                    case 1:
+                        g.FillRectangle(new SolidBrush(Color.Red), wall.getX() * 40, wall.getY() * 40, Wall.WALL_SIZE, Wall.WALL_SIZE);
+                        break;
+                    case 2:
+                        g.FillRectangle(new SolidBrush(Color.Yellow), wall.getX() * 40, wall.getY() * 40, Wall.WALL_SIZE, Wall.WALL_SIZE);
+                        break;
+                    case 3:
+                        g.FillRectangle(new SolidBrush(Color.Blue), wall.getX() * 40, wall.getY() * 40, Wall.WALL_SIZE, Wall.WALL_SIZE);
+                        break;
+                    default:
+                        break;
+
+                }
+                
             }
             
+        }
+
+        public void crateWall()
+        {
+            int mapHeight = getMapHeight();
+            int mapWidth = getMapWidth();
+            int mapSizeWidth = mapWidth / 40;
+            int mapSizeHeight = mapHeight / 40;
+
+            Random ran = new Random();
+            while(wallList.Count() != 20)
+            {
+
+                int x = ran.Next(mapSizeWidth);
+                int y = ran.Next(mapSizeHeight);
+                int type = ran.Next(4);
+                Wall wall = new Wall();
+                wall.setX(x);
+                wall.setY(y);
+                wall.setType(type);
+                if (isInSelf(wall))
+                {
+                    continue;
+                }
+                wallList.Add(wall);
+                }
+        }
+
+        //这里设置为private
+        //因为出错 参数类型权限与函数权限
+        //Wall中某些参数是private的
+        private Boolean isInSelf(Wall wallSelf)
+        {
+            foreach (Wall wall in wallList)
+            {
+                if (wallSelf.getX() == wall.getX() && wallSelf.getY() == wall.getY())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public int getMapHeight()
+        {
+            return this.Height;
+        }
+        public int getMapWidth()
+        {
+            return this.Width;
         }
     }
 }
--- TankDemo/Wall.cs
+++ TankDemo/Wall.cs
@@ -12,6 +12,11 @@ namespace TankDemo
         private int x;
         private int y;
 
+        //type 代表Wall类型
+        //0   1   2   3
+        //绿  红  黄   蓝
+        private int type;
+
         public int getX()
         {
             return x;
@@ -28,6 +33,15 @@ namespace TankDemo
         public void setY(int y)
         {
             this.y = y;
+        }
+        public int getType()
+        {
+            return type;
+        }
+
+        public void setType(int type)
+        {
+            this.type = type;
         }
     }
 }
